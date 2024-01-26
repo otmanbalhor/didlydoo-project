8627e357c656b17c025476c553ec9e2968080892
Aurelien229 <blampainaurelien229@gmail.com>
2024-01-26 08:56:31 +0100

    addDeleteFile

--- assets/modulejs/header.js
+++ assets/modulejs/header.js
@@ -6,14 +6,16 @@ document.addEventListener('DOMContentLoaded', function () {
         event.preventDefault();
         validateForm()
             .then(function (data) {
-                console.log("Données envoyées avec succès à l'API :", data);
+                console.log("Data successfully sent to API :", data);
                 updateUI(data);
                 resetForm();
+                location.reload();
             })
             .catch(function (error) {
-                console.error("Erreur :", error);
+                console.error("error :", error);
             });
     });
+    ;
 
     addDateBtn.addEventListener('click', function () {
         addDateField();
@@ -26,10 +28,20 @@ async function validateForm() {
         const description = document.getElementById('inputDescription').value;
         const author = document.getElementById('inputAuthor').value;
 
+        // Vérifier si la longueur de l'entrée dépasse 256 caractères
+        if (name.length > 256 || description.length > 256 || author.length > 256) {
+            alert("error : The length of the entry must not exceed 256 characters.");
+            return;
+        }
+
         // Récupérer les valeurs des champs de date
         const dateInputs = document.querySelectorAll('.header__div__date');
         const dates = Array.from(dateInputs).map(input => input.value);
-
+        // Vérifier si des dates identiques existent
+        if (hasDuplicates(dates)) {
+            alert("error : Identical dates have been selected.");
+            return; 
+        }
         const formData = {
             name: name,
             description: description,
@@ -37,7 +49,7 @@ async function validateForm() {
             dates: dates
         };
 
-        console.log("Données du formulaire :", formData);
+        console.log("Form data:", formData);
 
         const response = await fetch("http://localhost:3000/api/events", {
             method: "POST",
@@ -52,10 +64,13 @@ async function validateForm() {
             return data;
         } else {
             const responseBody = await response.text();
-            throw new Error(`Erreur lors de l'envoi des données à l'API. Statut : ${response.status}. Réponse : ${responseBody}`);
+            throw new Error(`Error sending data to API. Statut : ${response.status}. Réponse : ${responseBody}`);
         }
     } catch (error) {
         throw error;
+    }
+    function hasDuplicates(array) {
+        return new Set(array).size !== array.length;
     }
 }
 
@@ -63,11 +78,11 @@ function resetForm() {
     document.getElementById('inputTitle').value = '';
     document.getElementById('inputDescription').value = '';
     document.getElementById('inputAuthor').value = '';
-    
+
     // Supprimer tous les champs de date existants
     const dateContainer = document.getElementById('dates');
     const dateInputs = dateContainer.getElementsByClassName('header__div__date');
-    
+
     while (dateInputs.length > 0) {
         dateInputs[0].parentNode.removeChild(dateInputs[0]);
     }
@@ -75,10 +90,6 @@ function resetForm() {
     // Ajouter un champ de date initial avec la date actuelle
     addDateField();
 }
-
-
-
-
 
 function updateUI(eventData) {
     // Créer un ensemble pour stocker les dates uniques
@@ -91,9 +102,6 @@ function updateUI(eventData) {
 
     document.getElementById('app').innerHTML = row;
 }
-
-
-
 
 function addDateField() {
     // Créer un nouveau champ de date
